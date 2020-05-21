6baa067ce939d2c0765d2ffc5c3d22ebaf350cd4
Josh Fourie <josh.fourie97@gmail.com>
2020-05-21 18:27:17 +1000

    remove common_add
    Signed-off-by: Josh Fourie <josh.fourie97@gmail.com>

--- vector/src/lib.rs
+++ vector/src/lib.rs
@@ -204,32 +204,6 @@ macro_rules! ndarray {
         ndarray!(@common_additive_inv $length, $name, $space, $inner, $T);
     };
 
-    (@common_add $length:expr, $name:ident, $space:ident, $inner:ty, $T:ident) => {
-        impl<$T> VAdd for $space<$T>
-        where
-            for <'a> $T: Copy + AddAssign<&'a $T>,
-            for <'a> &'a $T: Add<&'a $T, Output=$T>
-        {
-            type Vector = $name<$T>;
-            
-            fn vadd_mut(&self, lhs: &mut Self::Vector, rhs: &Self::Vector)
-            {
-                lhs
-                    .0
-                    .iter_mut()
-                    .zip(rhs)
-                    .for_each(|(l,r)| l.add_assign(r));
-            }
-
-            fn vadd(&self, lhs: &Self::Vector, rhs: &Self::Vector) -> Self::Vector
-            {
-                let mut buf: Self::Vector = lhs.clone();
-                self.vadd_mut(&mut buf, rhs);
-                buf
-            }
-        }
-    };
-
     (@common_scale $length:expr, $name:ident, $space:ident, $inner:ty, $T:ident) => {
         impl<$T> VScale for $space<$T>
         where
