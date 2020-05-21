7b5395b4b2be412f99fa06274162e575c7663549
Josh Fourie <josh.fourie97@gmail.com>
2020-05-21 19:12:15 +1000

    add AdditiveInverseMut impl
    Signed-off-by: Josh Fourie <josh.fourie97@gmail.com>

--- vector/src/space/uniops.rs
+++ vector/src/space/uniops.rs
@@ -8,19 +8,27 @@ macro_rules! uniops {
         {
             type Vector = $name<$T>;
 
+            fn additive_inv(&self, vector: &Self::Vector) -> Self::Vector
+            {
+                let mut buf: Self::Vector = vector.clone();
+                self.additive_inv_mut(&mut buf);
+                buf
+            }
+        }
+
+        impl<$T> VAdditiveInverseMut for $space<$T>
+        where
+            $T: Copy,
+            for <'a> &'a $T: Neg<Output=$T>
+        {
+            type Vector = $name<$T>;
+
             fn additive_inv_mut(&self, vector: &mut Self::Vector)
             {
                 vector
                     .0
                     .iter_mut()
                     .for_each(|val| *val = (*val).neg() );
-            }
-
-            fn additive_inv(&self, vector: &Self::Vector) -> Self::Vector
-            {
-                let mut buf: Self::Vector = vector.clone();
-                self.additive_inv_mut(&mut buf);
-                buf
             }
         }
     };
