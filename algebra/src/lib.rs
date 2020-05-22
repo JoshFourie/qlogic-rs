b3bb5f626603fd2d6d1ab103c7acbf53327314ca
Josh Fourie <josh.fourie97@gmail.com>
2020-05-22 19:45:43 +1000

    add additive ident trait impls
    Signed-off-by: Josh Fourie <josh.fourie97@gmail.com>

--- algebra/src/lib.rs
+++ algebra/src/lib.rs
@@ -2,3 +2,4 @@ mod vector_space;
 pub use vector_space::*;
 
 mod num;
+pub use num::*;
--- algebra/src/num.rs
+++ algebra/src/num.rs
@@ -4,3 +4,85 @@ pub trait AdditiveIdent
 
     fn additive_ident() -> Self::Output;
 }
+
+#[macro_use]
+macro_rules! primitives {
+    (
+        $($primitive:ty),*
+    ) => {
+        $(
+            impl AdditiveIdent for $primitive
+            {
+                type Output = $primitive;
+
+                fn additive_ident() -> Self::Output
+                {
+                    0
+                }
+            }   
+            
+            impl<'a> AdditiveIdent for &'a $primitive
+            {
+                type Output = $primitive;
+
+                fn additive_ident() -> Self::Output
+                {
+                    0
+                }
+            } 
+
+            impl<'a> AdditiveIdent for &'a mut $primitive
+            {
+                type Output = $primitive;
+
+                fn additive_ident() -> Self::Output
+                {
+                    0
+                }
+            } 
+        )*
+    };
+
+    (
+        @float $($primitive:ty),*
+    ) => {
+        $(
+            impl AdditiveIdent for $primitive
+            {
+                type Output = $primitive;
+
+                fn additive_ident() -> Self::Output
+                {
+                    0.0
+                }
+            }   
+            
+            impl<'a> AdditiveIdent for &'a $primitive
+            {
+                type Output = $primitive;
+
+                fn additive_ident() -> Self::Output
+                {
+                    0.0
+                }
+            } 
+
+            impl<'a> AdditiveIdent for &'a mut $primitive
+            {
+                type Output = $primitive;
+
+                fn additive_ident() -> Self::Output
+                {
+                    0.0
+                }
+            } 
+        )*
+    };
+}
+
+primitives!{
+    u8, u16, u32, u64, u128, usize,
+    i8, i16, i32, i64, i128, isize
+}
+
+primitives!{@float f32, f64}
